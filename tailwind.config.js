85e5d91aadd59688f47c75de109e0605a0d11721
Vikas Sahni <vikassahni.tech@gmail.com>
2024-10-06 14:44:05 +0530

    completed quiz start section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
